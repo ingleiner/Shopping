adb1afd7d8e52ec864db22d5fc436248dd693430
Leiner Andrés Solano Brito <ingsisleinersolano@outlook.com>
2022-03-11 14:02:37 -0500

    Add Class Country

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        // Adicionar un Indice Único a la tabla Country
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
